1352b619718a32f07ea4f4d1bcb6d2d622ed77ba
Michael Madden <mikemadden42@icloud.com>
2018-12-17 21:48:51 -0600

    Make setup script non-interactive

--- setup_ansible_ubuntu.sh
+++ setup_ansible_ubuntu.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt-get install software-properties-common
+sudo apt-get install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt-get install -y ansible
-sudo apt-get install -y python-apt
+sudo apt-get install -y ansible python-apt
