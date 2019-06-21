300ea312636ae7e1ba9c0af3c9f8d0d4fbdb311c
Michael Madden <mikemadden42@icloud.com>
2019-06-21 13:09:05 -0500

    Add mtr & tcptraceroute

--- setup_ansible_ubuntu.sh
+++ setup_ansible_ubuntu.sh
@@ -3,4 +3,4 @@
 sudo apt-get update
 sudo apt-get install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt-get install -y ansible python-apt
+sudo apt-get install -y ansible aptitude python-apt
